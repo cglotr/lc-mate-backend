3120441abf12c1d7f71dcf751e4c2a31eaa275bc
cglotr <amir.ariffin.1992@gmail.com>
2022-06-04 13:24:30 +0800

    gomock

--- leetcode/leetcode_api.go
+++ leetcode/leetcode_api.go
@@ -1,5 +1,6 @@
 package leetcode
 
+//go:generate mockgen -package=leetcode -mock_names=LeetcodeApi=LeetcodeApiMock -source=./leetcode_api.go -destination=./leetcode_api_mock.go
 type LeetcodeApi interface {
 	GetUserInfo(username string) (*UserInfo, error)
 }
