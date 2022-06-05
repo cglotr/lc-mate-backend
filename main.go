306031d3f6db88619812df70a3981590a49e83ce
cglotr <amir.ariffin.1992@gmail.com>
2022-06-05 13:49:39 +0800

    refactor main

--- main.go
+++ main.go
@@ -19,9 +19,52 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func setupRoutes(r *gin.Engine, userService service.UserService) {
+	r.GET("/ping", controller.GetPingController())
+	r.GET("/user", controller.GetUserController(userService))
+	r.GET("/users", controller.GetUsersController(userService))
+}
+
 func main() {
 	godotenv.Load()
 
+	db, err := getDb()
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	userDaoImpl := dao.NewUserDaoImpl(db)
+	leetcodeApiImpl := leetcode.NewLeetcodeApiImpl(leetcode.BASE_URL)
+	userServiceImpl := service.NewUserServiceImpl(userDaoImpl, leetcodeApiImpl)
+
+	setupCron(userServiceImpl)
+	setupWebServer(userServiceImpl).Run()
+}
+
+func setupWebServer(userService service.UserService) *gin.Engine {
+	r := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"https://leetcode.com"}
+
+	r.Use(cors.New(config))
+	setupRoutes(r, userService)
+
+	return r
+}
+
+func setupCron(userService service.UserService) {
+	s := gocron.NewScheduler(time.UTC)
+	s.Every(10).Second().Do(func() {
+		user, err := userService.UpdateMostOutdatedUser()
+		if err != nil {
+			log.Printf("error updating user: %v\n", user.Username)
+		}
+	})
+	s.StartAsync()
+}
+
+func getDb() (*sql.DB, error) {
 	mysqlUsername := os.Getenv("MYSQL_USERNAME")
 	mysqlPassword := os.Getenv("MYSQL_PASSWORD")
 	mysqlIp := os.Getenv("MYSQL_IP")
@@ -34,41 +77,19 @@ func main() {
 
 	db, err := sql.Open("mysql", dataSourceName)
 	if err != nil {
-		log.Fatalln(err.Error())
+		return nil, err
 	}
 
 	err = db.Ping()
 	if err != nil {
-		log.Fatalln(err.Error())
+		return nil, err
 	}
 
 	migrationDir := "./migration"
 	err = arctictern.Migrate(db, migrationDir)
 	if err != nil {
-		log.Fatalln(err.Error())
+		return nil, err
 	}
 
-	userDaoImpl := dao.NewUserDaoImpl(db)
-	leetcodeApiImpl := leetcode.NewLeetcodeApiImpl(leetcode.BASE_URL)
-	userServiceImpl := service.NewUserServiceImpl(userDaoImpl, leetcodeApiImpl)
-
-	s := gocron.NewScheduler(time.UTC)
-	s.Every(10).Second().Do(func() {
-		user, err := userServiceImpl.UpdateMostOutdatedUser()
-		if err != nil {
-			log.Printf("error updating user: %v\n", user.Username)
-		}
-	})
-	s.StartAsync()
-
-	r := gin.Default()
-
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"https://leetcode.com"}
-
-	r.Use(cors.New(config))
-	r.GET("/ping", controller.GetPingController())
-	r.GET("/user", controller.GetUserController(userServiceImpl))
-	r.GET("/users", controller.GetUsersController(userServiceImpl))
-	r.Run()
+	return db, nil
 }
