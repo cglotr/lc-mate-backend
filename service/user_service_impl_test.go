4c59959064a41f0cd7a8ffe6f3aa0227b715e2c8
cglotr <amir.ariffin.1992@gmail.com>
2022-06-07 11:39:46 +0800

    complete service test

--- service/user_service_impl_test.go
+++ service/user_service_impl_test.go
@@ -12,6 +12,78 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestUpdateMostOutdatedUserUpsertError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	userDaoMock := dao.NewUserDaoMock(ctrl)
+	leetcodeApiMock := leetcode.NewLeetcodeApiMock(ctrl)
+	userServiceImpl := service.NewUserServiceImpl(userDaoMock, leetcodeApiMock)
+
+	userDaoMock.EXPECT().
+		QueryMostOutdatedUser().
+		Return(&model.UserModel{Username: "awice"}, nil)
+	leetcodeApiMock.EXPECT().
+		GetUserInfo(gomock.Eq("awice")).
+		Return(&leetcode.UserInfo{Username: "awice"}, nil)
+	userDaoMock.EXPECT().
+		Upsert(gomock.Eq(&model.UserModel{Username: "awice"})).
+		Return(errors.New(dao.ErrorUserNotFound()))
+
+	_, err := userServiceImpl.UpdateMostOutdatedUser()
+
+	assert.Equal(t, "error user not found", err.Error())
+}
+
+func TestErrorDeletingInvalidUser(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	userDaoMock := dao.NewUserDaoMock(ctrl)
+	leetcodeApiMock := leetcode.NewLeetcodeApiMock(ctrl)
+	userServiceImpl := service.NewUserServiceImpl(userDaoMock, leetcodeApiMock)
+
+	userDaoMock.EXPECT().
+		QueryMostOutdatedUser().
+		Return(&model.UserModel{Username: "awice"}, nil)
+	leetcodeApiMock.EXPECT().
+		GetUserInfo(gomock.Eq("awice")).
+		Return(nil, errors.New(service.ErrorInvalidUser()))
+	userDaoMock.EXPECT().
+		DeleteUser(gomock.Eq("awice")).
+		Return(errors.New(service.ErrorInvalidUser()))
+
+	_, err := userServiceImpl.UpdateMostOutdatedUser()
+
+	assert.Equal(t, "error invalid user", err.Error())
+}
+
+func TestUpdatingUserNotFound(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	userDaoMock := dao.NewUserDaoMock(ctrl)
+	leetcodeApiMock := leetcode.NewLeetcodeApiMock(ctrl)
+	userServiceImpl := service.NewUserServiceImpl(userDaoMock, leetcodeApiMock)
+
+	userDaoMock.EXPECT().
+		QueryMostOutdatedUser().
+		Return(nil, errors.New(dao.ErrorUserNotFound()))
+
+	_, err := userServiceImpl.UpdateMostOutdatedUser()
+
+	assert.Equal(t, "error user not found", err.Error())
+}
+
+func TestUserNotFound(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	userDaoMock := dao.NewUserDaoMock(ctrl)
+	leetcodeApiMock := leetcode.NewLeetcodeApiMock(ctrl)
+	userServiceImpl := service.NewUserServiceImpl(userDaoMock, leetcodeApiMock)
+
+	userDaoMock.EXPECT().
+		QueryUsers(gomock.Eq([]string{"awice"})).
+		Return(nil, errors.New(dao.ErrorUserNotFound()))
+
+	_, err := userServiceImpl.GetUsers([]string{"awice"})
+
+	assert.Equal(t, "error user not found", err.Error())
+}
+
 func TestDeletingInvalidUser(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	userDaoMock := dao.NewUserDaoMock(ctrl)
@@ -74,10 +146,10 @@ func TestMissingUsers(t *testing.T) {
 		Times(1)
 	userDaoMock.EXPECT().
 		Upsert(gomock.Eq(&model.UserModel{Username: "numb3r5"})).
-		Return(nil).
+		Return(errors.New(dao.ErrorUserNotFound())).
 		Times(1)
 	userDaoMock.EXPECT().
-		OutdateUser(gomock.Eq("awice")).Return(nil).
+		OutdateUser(gomock.Eq("awice")).Return(errors.New(dao.ErrorUserNotFound())).
 		Times(1)
 	userDaoMock.EXPECT().
 		OutdateUser(gomock.Eq("numb3r5")).Return(nil).
